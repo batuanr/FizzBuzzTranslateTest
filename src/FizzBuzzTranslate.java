339b5d7fc148b90626074b3669dbe70498a3b383
batuanr <tuanpesi@gmail.com>
2021-09-28 15:34:04 +0700

    pass test value 5

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,8 +1,8 @@
 public class FizzBuzzTranslate {
     public static String getTranslate(int number) {
-        if (number % 3 == 0 ) {
-            return "Fizz";
-        }
+        if (number % 5 == 0) return "Buzz";
+        if (number % 3 == 0 ) return "Fizz";
+
         return "o";
     }
 }
